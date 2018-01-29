c5e6cca27e431cb7590a3a4fea1e02f2b89a08d1
Hay Tran <haytranngoc@gmail.com>
2018-01-29 14:02:57 +0700

    create login

--- resources/views/layouts/header.blade.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom box-shadow">
-        <h5 class="my-0 mr-md-auto font-weight-normal">
-            <a href="#">@lang('messages.book')</a>
-        </h5>
-        <a class="btn btn-default" href="#">@lang('messages.login')</a>
-        <a class="btn btn-default" href="#">@lang('messages.register')</a>
-    </div>
-</nav>
